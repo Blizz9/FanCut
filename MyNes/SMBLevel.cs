2474c7500b7ca94d1be11f97b683edec3fee14e0
Nick <blizzard@gmail.com>
2015-12-04 20:00:46 -0500

    Moved logic to a separate file, still have to refactor to truly be separate

---
+++ MyNes/SMBLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyNes
+{
+    internal class SMBLevel
+    {
+        internal string Name { get; set; }
+        internal byte WorldNumber { get; set; }
+        internal byte LevelDisplayNumber { get; set; }
+        internal byte LevelNumber { get; set; }
+        internal byte AreaLoadedValue { get; set; }
+        internal byte CheckpointScreenNumber { get; set; }
+    }
+}
---
+++ MyNes/TimelineSave.cs
@@ -0,0 +1,10 @@
+﻿namespace MyNes
+{
+    internal class TimelineSave
+    {
+        internal int ID { get; set; }
+        internal string Name { get; set; }
+        internal string SaveStateName { get; set; }
+        internal string ThumbnailName { get; set; }
+    }
+}
